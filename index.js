90b0c0e58d03faef5bc72a6f9fb8edb96d27761c
4lexandrine <alexandrine.defoor@gmail.com>
2020-11-24 19:06:09 +0100

    cors

--- index.js
+++ index.js
@@ -4,7 +4,20 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 require('dotenv').config();
-app.use(cors());
+
+const whitelist = [process.env.ALLOWED_APPS]
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+
+
+app.use(cors(corsOptions));
 app.use(expressMiddleware());
 
 mongoose.connect(
